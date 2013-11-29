e24b17a90790d179b426f48f144f6c2eadd2a48d
Thomas Levine <occurrence@thomaslevine.com>
2013-11-29 06:31:18 +0000

    load csv

---
+++ run.r
@@ -0,0 +1,4 @@
+cost.of.living <- read.csv('12s0728.csv',
+  row.names = 1, header = TRUE,
+  col.names = c('urban.area', 'composite.index', 'grocery.items', 'housing', 'utilities', 'transportation', 'health.care', 'miscalaneous.goods.and.services')
+)
