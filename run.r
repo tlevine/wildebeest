051576e58fe3cc10eeb72e3e2d74b9a7f4811f97
Thomas Levine <occurrence@thomaslevine.com>
2013-11-29 06:34:22 +0000

    money

--- run.r
+++ run.r
@@ -2,3 +2,8 @@ cost.of.living <- read.csv('12s0728.csv',
   row.names = 1, header = TRUE,
   col.names = c('urban.area', 'composite.index', 'grocery.items', 'housing', 'utilities', 'transportation', 'health.care', 'miscalaneous.goods.and.services')
 )
+average.cost.of.living <- 3e4
+cost.of.living <- average.cost.of.living * cost.of.living / 100
+
+# cost.of.living[order(cost.of.living$composite.index, decreasing = TRUE),]
+
